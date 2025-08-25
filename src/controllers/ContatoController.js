9ebb418eec72838aaa030ed3e9be48e9342913f1
Instrutor-WagnerLeodoro <wagner.leodoro@docente.pr.senac.br>
2025-08-25 14:11:15 -0300

    Atualiza a página de contato para redirecionar para a nova página de agradecimento e ajusta títulos das páginas de serviços, portfólio e contato.

--- src/controllers/ContatoController.js
+++ src/controllers/ContatoController.js
@@ -6,18 +6,12 @@ module.exports = {
         res.sendFile(path.join(__dirname, "../views/contato.html"))
     },
     send: async (req, res) => {
-        const {nome, email, whatsapp, mensagem} = req.body;
-        console.log(req.body);
-        
+        const {nome, email, whatsapp, mensagem} = req.body;        
         try {
             await db("contatos").insert({nome, email, whatsapp, mensagem})
             console.log("📩 Novo contato salvo no banco de dados");
 
-            res.send(`
-                    <h1>Obrigado pelo seu contato, ${nome}</h1>
-                    <p>Sua mensagem foi registrada e em breve entraremos em contato.</p>
-                    <a href="http://localhost:3000">Voltar para Home</a>
-                `)
+            res.sendFile(path.join(__dirname, "../views/agradecimento.html"))
         } catch (error) {
             console.error("Erro ao salvar contato:" , error);
             res.status(500).send("Erro ao enviar mensagem. Tente novamente mais tarde.")
